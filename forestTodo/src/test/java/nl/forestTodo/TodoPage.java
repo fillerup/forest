dfb724d51f65e8b31a798927c8176d8fa6de2ab2
fillerup <mats.fillerup@gmail.com>
2014-12-14 14:52:25 +0100

    A simple todo page, not yet finished.

--- forestTodo/src/test/java/nl/forestTodo/TodoPage.java
+++ forestTodo/src/test/java/nl/forestTodo/TodoPage.java
@@ -4,26 +4,29 @@ import org.jbehave.core.annotations.Aliases;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.model.Lifecycle;
+import org.jbehave.core.steps.Steps;
 
 public class TodoPage {
 
     @When("I go to the todo page")
     public void openTodoPage() {
-        //webdriver stuff goes in here.
+        System.out.println("Test 1");
     }
 
     @When("I check the first item on the list")
     public void checkFirstItem() {
-        //webdriver stuff goes in here.
+        System.out.println("Test 2");
     }
 
     @Then("I see the 14 todo items")
-    public void getNumberOf(int grid) {
-        assertThat(renderer.asString(), equalTo(grid));
+    public void getNumberOfTodos(int grid) {
+        //webdriver stuff goes in here.
     }
 
     @Then("The first item disapears from the list")
-    public void theGridShouldLookLike(String grid) {
-        assertThat(renderer.asString(), equalTo(grid));
+    public void getFirstTodoVisible() {
+        //webdriver stuff goes in here.
     }
 }
